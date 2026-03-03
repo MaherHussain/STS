c1137b59250ce4634cd80f26db3af6b743991107
Maher Hussain <maherhussain6@gmail.com>
2026-03-03 11:12:21 +0100

    chore: add toast notification message

--- frontend/vite-project/src/main.tsx
+++ frontend/vite-project/src/main.tsx
@@ -6,11 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./utils/providers/AuthProvider.tsx";
 import ReactQueryProvider from "./utils/providers/ReactQueryProvider.tsx";
 
+import { Toaster } from "react-hot-toast";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReactQueryProvider>
       <BrowserRouter>
         <AuthProvider>
+          <Toaster position="top-center" reverseOrder={false} />
           <App />
         </AuthProvider>
       </BrowserRouter>
