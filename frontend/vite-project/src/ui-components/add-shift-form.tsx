6372dbfd8f733e473f39e8d9888de16e2586e8a7
Maher Hussain <maherhussain6@gmail.com>
2026-04-10 23:02:35 +0200

    disable date input

--- frontend/vite-project/src/ui-components/add-shift-form.tsx
+++ frontend/vite-project/src/ui-components/add-shift-form.tsx
@@ -132,6 +132,7 @@ export default function AddShiftLogModal({
               type="date"
               value={date}
               readOnly
+              disabled
               className="w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
             />
           </div>
